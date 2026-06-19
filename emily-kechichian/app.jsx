fe5f03846a30f07933591729e1df7f297379dcee
jeffry <thisisjeffry77@gmail.com>
2026-06-20 01:43:32 +0300

    font changes

--- emily-kechichian/app.jsx
+++ emily-kechichian/app.jsx
@@ -8,10 +8,10 @@ export default function Main()  {
       <img src={emily} />
       <div className="description">
         <h1>Emily Kechichian 🔥</h1>
-        <h2>insta: <a href="https://www.instagram.com/emilykechichian/"  target="_blank">emilykechichian</a></h2>
-        <h2>Tiktok: <a href="https://www.tiktok.com/@emilykechichian"  target="_blank">emilykechichian</a></h2>
-        <h2>Phone Number: <a style={{textDecoration: "underline"}} href="#">+961 76 634  586</a></h2>
-        <h4>Credits to Jeffry for the website</h4>
+        <h3 style={{fontWeight:"700"}}>insta: <a href="https://www.instagram.com/emilykechichian/"  target="_blank">emilykechichian</a></h3>
+        <h3>Tiktok: <a href="https://www.tiktok.com/@emilykechichian"  target="_blank">emilykechichian</a></h3>
+        <h3>Phone Number: <a style={{textDecoration: "underline"}} href="#">+961 76 634  586</a></h3>
+        <h5>Credits to Jeffry for the website</h5>
       </div>
       
     </main>
