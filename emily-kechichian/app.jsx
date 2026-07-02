76d3c83604257dbbc39a4d432e214408d2ce05d4
jeffry <thisisjeffry77@gmail.com>
2026-07-02 14:06:22 +0300

    minor UI changes

--- emily-kechichian/app.jsx
+++ emily-kechichian/app.jsx
@@ -10,8 +10,7 @@ export default function Main()  {
         <h1>Emily Kechichian 🔥</h1>
         <h3 style={{fontWeight:"700"}}>insta: <a href="https://www.instagram.com/emilykechichian/"  target="_blank">emilykechichian</a></h3>
         <h3>Tiktok: <a href="https://www.tiktok.com/@emilykechichian"  target="_blank">emilykechichian</a></h3>
-        <h3>Phone Number: <a style={{textDecoration: "underline"}} href="#">+961 76 634  586</a></h3>
-        <h5>Credits to Jeffry for the website</h5>
+        <h5>Credits to <a target="_blank" href="https://www.tiktok.com/@itsjeffry3">Jeffry</a> for the website</h5>
       </div>
       
     </main>
